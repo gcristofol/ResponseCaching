c06ad48ea6d8bb0322ed35a92686d125680108a1
John Luo <jt.luo@mail.utoronto.ca>
2016-11-23 18:59:38 -0800

    Initialize HeaderDictionary lazily

--- src/Microsoft.AspNetCore.ResponseCaching/Internal/CacheEntry/CachedResponse.cs
+++ src/Microsoft.AspNetCore.ResponseCaching/Internal/CacheEntry/CachedResponse.cs
@@ -9,11 +9,31 @@ namespace Microsoft.AspNetCore.ResponseCaching.Internal
 {
     public class CachedResponse : IResponseCacheEntry
     {
+        private IHeaderDictionary _headers;
+        private bool _headersInitilized = false;
+
         public DateTimeOffset Created { get; set; }
 
         public int StatusCode { get; set; }
 
-        public IHeaderDictionary Headers { get; set; } = new HeaderDictionary();
+        public IHeaderDictionary Headers
+        {
+            get
+            {
+                if (!_headersInitilized && _headers == null)
+                {
+                    _headersInitilized = true;
+                    _headers = new HeaderDictionary();
+                }
+                return _headers;
+            }
+            set
+            {
+                // Don't initialized header dictionary if it's explicitly set
+                _headersInitilized = true;
+                _headers = value;
+            }
+        }
 
         public Stream Body { get; set; }
     }
