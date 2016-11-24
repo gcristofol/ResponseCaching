ebd7d44c14885a10dedd8b4844c042c5501f1e3e
John Luo <jt.luo@mail.utoronto.ca>
2016-11-23 17:53:49 -0800

    Convert to long before calling ToString

--- src/Microsoft.AspNetCore.ResponseCaching/ResponseCachingMiddleware.cs
+++ src/Microsoft.AspNetCore.ResponseCaching/ResponseCachingMiddleware.cs
@@ -142,7 +142,7 @@ namespace Microsoft.AspNetCore.ResponseCaching
                         response.Headers.Add(header);
                     }
 
-                    response.Headers[HeaderNames.Age] = context.CachedEntryAge.Value.TotalSeconds.ToString("F0", CultureInfo.InvariantCulture);
+                    response.Headers[HeaderNames.Age] = ((long)context.CachedEntryAge.Value.TotalSeconds).ToString("F", CultureInfo.InvariantCulture);
 
                     // Copy the cached response body
                     var body = context.CachedResponse.Body;
